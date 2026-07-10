e4aa0c7671e82942f97f87393211286a3ac0821a
MalakaRM <160582947+MalakaRM@users.noreply.github.com>
2026-07-11 00:16:53 +0530

    rest,spread opeartor & date time in js

--- main.js
+++ main.js
@@ -1,76 +1,36 @@
-/*let name = 'Nimal';
-//console.log(name[1]); 
+  //---rest-- 
+ /* let numbers=[1,2,3,4,5];
+  const[first,second,...rest]=numbers;
+  console.log(first);
+  console.log(second);
+  console.log(rest);
 
-let name1 = 'B'; // This will not change the value of name because strings are immutable in JavaScript
-console.log(name); // Output: Nimal 
+  //---spread--
+  let ages=[20,30,40];
+  let newAges=[...ages,50,60];
+  console.log(newAges); 
 
-//=====Immutable Strings in JavaScript===== 
-let x = name.replace('N', 'V'); // This will create a new string with the replaced value
-console.log(x); // Output: Vimal*/
+  //--dates and times--
+  let date=new Date();
+  console.log(date);
 
+  let date1=new Date("2020-10-20");
+  console.log(date1);
 
+  let date2=new Date();
+  console.log(date2.toISOString().split("T")[0]);
 
-
-
-
-/*let story = 'java script hrima lesi';
-
-let pikedText = story.slice(0, 11); // This will extract the substring from index 0 to 10 (not including 10)
-console.log(pikedText); 
-
-let replacedText = story.replace('hrima', 'is'); // This will replace the first occurrence of 'hrima' with 'is'
-console.log(replacedText);
-
-let upperCaseText = story.toUpperCase(); // This will convert the entire string to uppercase
-console.log(upperCaseText);*/  
-
-
-/*-----ES6 Features-----*/
-
-//let & const are block-scoped variables introduced in ES6. They provide better scoping rules compared to var, which is function-scoped.
-/*let x = 10;
-let y = 20;
-
-if(true){
-    let x = 30; // This x is block-scoped and will not affect the outer x
-    let y = 40;
-    console.log(x); // Output: 30
-    console.log(y); // Output: 40
-}
-console.log(x); // Output: 10
-console.log(y); // Output: 20
-
-//Arrow functions are a concise way to write functions in ES6. They have a shorter syntax and do not have their own 'this' context.
-let calculateSum = (a, b) => a + b; // This is an arrow function that takes two parameters and returns their sum
-console.log(calculateSum(5, 10)); // Output: 15
-
-//Template literals are a new way to create strings in ES6. They allow for multi-line strings and string interpolation using backticks (`) and ${} syntax.
-let name = 'Nimal';
-let age = 25;
-let message = `My name is ${name} and I am ${age} years old.`;
-console.log(message); // Output: My name is Nimal and I am 25 years old. */
-
-  /*let age = 25;
-  let name = 'Nimal';
-
-  let student={
-    age,
-    name,
-    printAge(){
-        console.log(`my age is ${age}`);
-    }
-  };
-  console.log(student.age); // Output: 25
-  console.log(student.name);
-  student.printAge(); // Output: my age is 25*/
-
-  //--classes--
-  class Student {
-    constructor(name, age) {
-      this.name = name;
-      this.age = age;
-    }   
-}
-let data = new Student('Nimal', 25);
-console.log(data.name); // Output: Nimal
-console.log(data.age); // Output: 25
+  const curDate = new Date();
+  console.log(curDate.getFullYear());
+  console.log(curDate.getMonth()+1);    
+  console.log(curDate.getDate());
+  console.log(curDate.getDay());
+  console.log(curDate.getHours());
+  console.log(curDate.getMinutes());
+  console.log(curDate.getSeconds());
+  */
+  //--operators--
+  const curDate = new Date();
+  console.log(curDate.toDateString());
+   console.log(curDate.getDate()+7);
+   //mils->dif
